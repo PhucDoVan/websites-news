ca82b50f1cbdb156e5b40e3c9442341065a6166e
PhucDoVan <dvphuc.sgt@gmail.com>
2020-06-09 23:33:09 +0700

    #5 code update Admin Screen

--- server/routes/web.php
+++ server/routes/web.php
@@ -16,3 +16,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Admin route
+Route::prefix('admin')->name('admin.')->group(function () {
+//    Route::get('login', 'Admin\Auth\LoginController@getLogin')->name('login');
+//    Route::post('login', 'Admin\Auth\LoginController@postLogin');
+    Route::get('dashboard', 'Admin\DashboardController@index')->name('dashboard');
+    Route::group(['middleware' => ['checkLoginUser', 'preventBackButton']], function () {
+
+        Route::get('logout', 'Admin\Auth\LoginController@getLogout');
+
+        Route::get('shop', 'Admin\ShopController@index')->name('shop');
+        Route::get('top', 'Admin\TopController@getListSlider')->name('top');
+        Route::get('site', 'Admin\SiteController@index')->name('site');
+        Route::get('event', 'Admin\EventController@list')->name('event');
+        Route::get('news', 'Admin\NewsController@list')->name('news');
+        Route::get('staff', 'Admin\StaffController@list')->name('staff');
+        Route::get('contact', 'Admin\ContactController@list')->name('contact');
+
+        Route::prefix('event')->name('event.')->group(function () {
+            Route::get('list', 'Admin\EventController@list')->name('list');
+            Route::get('detail', 'Admin\EventController@detail')->name('detail');
+        });
+        Route::prefix('news')->name('news.')->group(function () {
+            Route::get('list', 'Admin\NewsController@list')->name('list');
+            Route::get('detail', 'Admin\NewsController@detail')->name('detail');
+        });
+        Route::prefix('staff')->name('staff.')->group(function () {
+            Route::get('list', 'Admin\StaffController@list')->name('list');
+            Route::get('detail', 'Admin\StaffController@detail')->name('detail');
+        });
+        Route::prefix('top')->name('top.')->group(function () {
+            Route::post('destroy', 'Admin\TopController@deleteSliders')->name('destroy');
+        });
+        Route::prefix('contact')->name('contact.')->group(function () {
+            Route::get('list', 'Admin\ContactController@list')->name('list');
+            Route::get('detail', 'Admin\ContactController@detail')->name('detail');
+        });
+    });
+});
+
